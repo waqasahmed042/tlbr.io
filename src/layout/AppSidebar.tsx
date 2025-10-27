5cade8a6b749470e6bafa5675d4ecf496b4b3a27
waqasahmed042 <174671755+waqasahmed042@users.noreply.github.com>
2025-10-27 15:51:09 +0500

    fix overlay issue

--- src/layout/AppSidebar.tsx
+++ src/layout/AppSidebar.tsx
@@ -34,9 +34,10 @@ const AppSidebar: React.FC = () => {
   return (
     <>
       <aside
-        className={`fixed mt-16 flex flex-col lg:mt-0 top-0 px-5 left-0 bg-white dark:bg-gray-900 dark:border-gray-800 text-gray-900 h-screen transition-all duration-300 ease-in-out z-10 border-r border-gray-200 
-        ${isExpanded || isMobileOpen ? "w-[290px]" : isHovered ? "w-[290px]" : "w-[90px]"}
-        ${isMobileOpen ? "translate-x-0" : "-translate-x-full"} lg:translate-x-0`}
+        className={`fixed mt-16 flex flex-col lg:mt-0 top-0 px-5 left-0 bg-white dark:bg-gray-900 dark:border-gray-800 text-gray-900 h-screen transition-all duration-300 ease-in-out border-r border-gray-200 
+          ${isExpanded || isMobileOpen ? "w-[290px]" : isHovered ? "w-[290px]" : "w-[90px]"}
+          ${isMobileOpen ? "translate-x-0" : "-translate-x-full"} lg:translate-x-0 
+          ${isMobileOpen ? "z-50" : "z-10"}`}
         onMouseEnter={() => !isExpanded && setIsHovered(true)}
         onMouseLeave={() => setIsHovered(false)}
       >
